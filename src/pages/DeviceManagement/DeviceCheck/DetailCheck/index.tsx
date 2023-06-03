9a7c181d4e8ad1f5cd1b4252431ad73feb1a0113
lzm-huster <815925407@qq.com>
2023-06-03 16:34:11 +0800

    0603 界面框架

---
+++ src/pages/DeviceManagement/DeviceCheck/DetailCheck/index.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from '@ant-design/pro-components';
+
+const DetailCheck: React.FC = () => {
+  return (
+    <PageContainer>
+      <div></div>
+    </PageContainer>
+  );
+};
+export default DetailCheck;
