ff61602a790c39a76d5094b7265fb3c35f53703d
lzm-huster <815925407@qq.com>
2023-06-16 21:13:49 +0800

    0616 小问题处理

--- src/services/general/dataProcess.ts
+++ src/services/general/dataProcess.ts
@@ -49,16 +49,6 @@ export function convertToSelectData(assets: Asset[]) {
   return selectData;
 }
 
-export function formatDate(date: Date) {
-  const year = date.getFullYear();
-  const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  const day = ('0' + date.getDate()).slice(-2);
-  const hours = ('0' + date.getHours()).slice(-2);
-  const minutes = ('0' + date.getMinutes()).slice(-2);
-  const seconds = ('0' + date.getSeconds()).slice(-2);
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
 export function getMonth1st(monthNum: number) {
   let data = new Date();
   const li = [];
